004a23fe5ae38a14b92e0262e1295da3660c9822
Jonathan <jonacris2007@gmail.com>
2020-11-24 20:12:18 -0400

    update pagina principal

--- index.php
+++ index.php
@@ -32,39 +32,262 @@
     </div>
 
     <div class="container">
-        <div class="row mt-3 justify-content-center">
-          <div id="publicidad-um" class="col-sm-5 mr-3">
+        <div class="row mt-3 justify-content-between">
+          <div id="publicidad-um" class="col-sm-5">
             
           </div>
-          <div id="publicidad-um" class="col-sm-3 mr-2">
+          <div id="publicidad-um" class="col-sm-3">
             One of three columns
           </div>
-          <div id="publicidad-um" class="col-sm-3 ml-2">
+          <div id="publicidad-um" class="col-sm-3">
             One of three columns
+          </div>
+        </div>
+
+        <div class="row mt-3 justify-content-between">
+            <div id="publicidad-um" class="col-sm-3"></div>
+            <div id="publicidad-um" class="col-sm-3">
+                One of three columns
+            </div>
+            <div id="publicidad-um" class="col-sm-5">
+                One of three columns
+            </div>
+        </div>
+
+        <div id="info" class="mt-3 col-sm-12">
+            <h5 class="ml-3 font-weight-bold">SOLO POR HORAS</h5>
+        </div>
+
+        <div id="oferta" class="justify-content-center align-items-center">
+          <div id="carrusell" class="row col-sm-10 justify-content-around">
+              <div id="producto" class="col-sm-3 justify-content-center flex-column align-items-center">
+                <div id="imagen-p"></div>
+                <label for="" class="mt-1 font-weight-bold">Lorem Ipsum</label>
+                <label for="" class="mt-1 font-weight-bold">200$</label>
+                <button type="button" class="btn">
+                  <label for=""><i class="fas fa-shopping-cart fa-1x"></i> Agregar</label>
+                </button>
+              </div>
+              <div id="producto" class="col-sm-3 justify-content-center flex-column align-items-center">
+                <div id="imagen-p"></div>
+                <label for="" class="mt-1 font-weight-bold">Lorem Ipsum</label>
+                <label for="" class="mt-1 font-weight-bold">200$</label>
+                <button type="button" class="btn">
+                  <label for=""><i class="fas fa-shopping-cart fa-1x"></i> Agregar</label>
+                </button>
+              </div>
+              <div id="producto" class="col-sm-3 justify-content-center flex-column align-items-center">
+                <div id="imagen-p"></div>
+                <label for="" class="mt-1 font-weight-bold">Lorem Ipsum</label>
+                <label for="" class="mt-1 font-weight-bold">200$</label>
+                <button type="button" class="btn">
+                  <label for=""><i class="fas fa-shopping-cart fa-1x"></i> Agregar</label>
+                </button>
+              </div>
+              <div id="producto" class="col-sm-3 justify-content-center flex-column align-items-center">
+                <div id="imagen-p"></div>
+                <label for="" class="mt-1 font-weight-bold">Lorem Ipsum</label>
+                <label for="" class="mt-1 font-weight-bold">200$</label>
+                <button type="button" class="btn">
+                  <label for=""><i class="fas fa-shopping-cart fa-1x"></i> Agregar</label>
+                </button>
+              </div>
           </div>
         </div>
     </div>
 
-    <div class="container">
-        <div class="row mt-3 justify-content-center">
-            <div id="publicidad-um" class="col-sm-3 mr-3"></div>
-            <div id="publicidad-um" class="col-sm-3 mr-2">
-                One of three columns
-            </div>
-            <div id="publicidad-um" class="col-sm-5 ml-2">
-                One of three columns
-            </div>
-        </div>
+    <div id="span" class="container">
+      <p class="col-sm-10 mt-4 font-weight-bold">De todo en tecnologia a unos ofertones</p>
     </div>
 
     <div class="container">
-        <div id="info" class="row mt-3">
-            <h5 class="justify-content-start">SOLO POR HORAS</h5>
+      <div id="oferta" class="justify-content-center align-items-center mb-5">
+        <div id="producto" class="col-sm-3 justify-content-center flex-column align-items-center">
+          <div id="imagen-p"></div>
+          <label for="" class="mt-1 font-weight-bold">Lorem Ipsum</label>
+          <label for="" class="mt-1 font-weight-bold">200$</label>
+          <button type="button" class="btn">
+            <label for=""><i class="fas fa-shopping-cart fa-1x"></i> Agregar</label>
+          </button>
         </div>
+        <div id="producto" class="col-sm-3 justify-content-center flex-column align-items-center">
+          <div id="imagen-p"></div>
+          <label for="" class="mt-1 font-weight-bold">Lorem Ipsum</label>
+          <label for="" class="mt-1 font-weight-bold">200$</label>
+          <button type="button" class="btn">
+            <label for=""><i class="fas fa-shopping-cart fa-1x"></i> Agregar</label>
+          </button>
+              </div>
+        <div id="producto" class="col-sm-3 justify-content-center flex-column align-items-center">
+          <div id="imagen-p"></div>
+          <label for="" class="mt-1 font-weight-bold">Lorem Ipsum</label>
+          <label for="" class="mt-1 font-weight-bold">200$</label>
+          <button type="button" class="btn">
+            <label for=""><i class="fas fa-shopping-cart fa-1x"></i> Agregar</label>
+          </button>
+        </div>
+        <div id="producto" class="col-sm-3 justify-content-center flex-column align-items-center">
+          <div id="imagen-p"></div>
+          <label for="" class="mt-1 font-weight-bold">Lorem Ipsum</label>
+          <label for="" class="mt-1 font-weight-bold">200$</label>
+          <button type="button" class="btn">
+            <label for=""><i class="fas fa-shopping-cart fa-1x"></i> Agregar</label>
+          </button>
+        </div>
+      </div>
     </div>
+
+    <div class="container">
+      <div class="row mt-3 justify-content-between">
+        <div id="publicidad-um" class="col-sm-5">
+            
+        </div>
+        <div id="publicidad-um" class="col-sm-3">
+            One of three columns
+        </div>
+        <div id="publicidad-um" class="col-sm-3">
+            One of three columns
+        </div>
+      </div>
+    </div>
+
+    <div id="span" class="container">
+      <p class="col-sm-10 mt-4 font-weight-bold">Variedad en Licores</p>
+    </div>
+
+    <div class="container">
+      <div id="oferta" class="justify-content-center align-items-center mb-5">
+        <div id="producto" class="col-sm-3 justify-content-center flex-column align-items-center">
+          <div id="imagen-p"></div>
+          <label for="" class="mt-1 font-weight-bold">Lorem Ipsum</label>
+          <label for="" class="mt-1 font-weight-bold">200$</label>
+          <button type="button" class="btn">
+            <label for=""><i class="fas fa-shopping-cart fa-1x"></i> Agregar</label>
+          </button>
+        </div>
+        <div id="producto" class="col-sm-3 justify-content-center flex-column align-items-center">
+          <div id="imagen-p"></div>
+          <label for="" class="mt-1 font-weight-bold">Lorem Ipsum</label>
+          <label for="" class="mt-1 font-weight-bold">200$</label>
+          <button type="button" class="btn">
+            <label for=""><i class="fas fa-shopping-cart fa-1x"></i> Agregar</label>
+          </button>
+              </div>
+        <div id="producto" class="col-sm-3 justify-content-center flex-column align-items-center">
+          <div id="imagen-p"></div>
+          <label for="" class="mt-1 font-weight-bold">Lorem Ipsum</label>
+          <label for="" class="mt-1 font-weight-bold">200$</label>
+          <button type="button" class="btn">
+            <label for=""><i class="fas fa-shopping-cart fa-1x"></i> Agregar</label>
+          </button>
+        </div>
+        <div id="producto" class="col-sm-3 justify-content-center flex-column align-items-center">
+          <div id="imagen-p"></div>
+          <label for="" class="mt-1 font-weight-bold">Lorem Ipsum</label>
+          <label for="" class="mt-1 font-weight-bold">200$</label>
+          <button type="button" class="btn">
+            <label for=""><i class="fas fa-shopping-cart fa-1x"></i> Agregar</label>
+          </button>
+        </div>
+      </div>
+    </div> 
+    
+    <div id="span" class="container">
+      <h3 class="col-sm-10 mt-4 font-weight-bold">Mundo Ucab Market</h3>
+    </div>
+
+    <div class="container">
+      <div id="seccion-propia-um" class=" col-sm-12 mt-3">
+        <div id="producto-um" class="col-sm-2 flex-column justify-content-center align-items-center">
+          <div id="circle-um"></div>
+          <label for="" class="mt-1 font-weight-bold text-center">Productos Umarket</label>
+        </div>
+        <div id="producto-um" class="col-sm-2 flex-column justify-content-center align-items-center">
+          <div id="box-um"></div>
+          <label for="" class="mt-1 font-weight-bold">Navidad</label>
+        </div>
+        <div id="producto-um" class="col-sm-2 flex-column justify-content-center align-items-center">
+          <div id="box-um"></div>
+          <label for="" class="mt-1 font-weight-bold">Catalogos</label>
+        </div>
+        <div id="producto-um" class="col-sm-2 flex-column justify-content-center align-items-center">
+          <div id="box-um"></div>
+          <label for="" class="mt-1 font-weight-bold">Oferta</label>
+        </div>
+        <div id="producto-um" class="col-sm-2 flex-column justify-content-center align-items-center">
+          <div id="box-um"></div>
+          <label for="" class="mt-1 font-weight-bold">Linea Blanca</label>
+        </div>
+        <div id="producto-um" class="col-sm-2 flex-column justify-content-center align-items-center">
+          <div id="box-um"></div>
+          <label for="" class="mt-1 font-weight-bold">Deportes</label>
+        </div>
+      </div>
+    </div>
+    
+    <div class="container">
+      <div id="seccion-propia-um" class=" col-sm-12 mt-3">
+        <div id="producto-um" class="col-sm-2 flex-column justify-content-center align-items-center">
+          <div id="box-um"></div>
+          <label for="" class="mt-1 font-weight-bold text-center">Licores</label>
+        </div>
+        <div id="producto-um" class="col-sm-2 flex-column justify-content-center align-items-center">
+          <div id="box-um"></div>
+          <label for="" class="mt-1 font-weight-bold">Televisores</label>
+        </div>
+        <div id="producto-um" class="col-sm-2 flex-column justify-content-center align-items-center">
+          <div id="box-um"></div>
+          <label for="" class="mt-1 font-weight-bold">Belleza</label>
+        </div>
+        <div id="producto-um" class="col-sm-2 flex-column justify-content-center align-items-center">
+          <div id="box-um"></div>
+          <label for="" class="mt-1 font-weight-bold">Mascotas</label>
+        </div>
+        <div id="producto-um" class="col-sm-2 flex-column justify-content-center align-items-center">
+          <div id="box-um"></div>
+          <label for="" class="mt-1 font-weight-bold">Tecnologia</label>
+        </div>
+        <div id="producto-um" class="col-sm-2 flex-column justify-content-center align-items-center">
+          <div id="box-um"></div>
+          <label for="" class="mt-1 font-weight-bold">Moda</label>
+        </div>
+      </div>
+    </div>
+
+    <div id="condiciones" class="container">
+      <p class="col-sm-10 mt-5 font-weight-bold">¿Que productos puedo encontrar en la tienda online Ucabmarket.com?</p>
+      <p class="col-sm-10 mt-1 font-weight-bold">
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolor voluptate deserunt odio quos eius esse perferendis
+        sint harum! Deserunt aperiam quidem earum ipsa sint voluptatem perspiciatis ducimus tenetur eius accusantium!Eius 
+        cupiditate placeat a accusamus molestiae, iure dicta rerum labore consequatur culpa perspiciatis ipsa modi enim 
+        corrupti repellendus velit natus accusantium in voluptate sit! Alias autem cumque perspiciatis architecto maiores?
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit debitis aliquid consequatur officia natus? Quos, 
+        praesentium! Praesentium fugit obcaecati inventore, culpa harum, adipisci dolore sed labore qui odit possimus eaque.
+        Asperiores eaque dolores et maxime blanditiis optio, cumque atque nisi, minus quaerat doloremque rerum itaque sequi 
+        nesciunt provident, voluptatibus nam voluptate ad? Voluptatum maiores asperiores quaerat culpa odio placeat ad!
+        Culpa impedit magni quidem quae, quas commodi nihil provident voluptatum minus praesentium recusandae cumque tempore 
+        ipsa at aperiam asperiores voluptatibus dicta in cum repellat quo officia qui. Officia, repellat aut.
+        Provident illo aperiam quo expedita perspiciatis iure, fugiat temporibus eligendi nemo minus eveniet maxime voluptate 
+        commodi doloribus quod odit consequuntur cupiditate reiciendis impedit quasi facilis? Esse explicabo quaerat ipsum soluta!
+        Voluptatibus quo asperiores iusto minus, laboriosam odit placeat sapiente quaerat neque quidem consequuntur deserunt 
+        ipsa soluta voluptas assumenda ad doloremque omnis et nobis maiores harum cupiditate pariatur praesentium alias? Inventore?
+        Perspiciatis officiis molestias harum voluptates aperiam accusamus laudantium facilis ad vitae odio debitis voluptatem 
+        suscipit fugiat quibusdam ducimus ratione dolorum atque illum beatae, impedit doloremque doloribus et, maiores eveniet! Eos?
+        Facere quod iusto, voluptas voluptates possimus repudiandae, dicta excepturi optio dolor earum eos, magni non sapiente 
+        vitae. Ipsum, nihil repellendus perspiciatis ducimus iste iusto dolores est, pariatur eveniet saepe amet.
+        Veniam porro ratione tempora. Laboriosam consequatur commodi eius dolor nesciunt magnam, rerum esse cum harum deleniti 
+        sed enim sint, porro cumque blanditiis. Non commodi harum corrupti deleniti, sit cumque facere?
+        Laboriosam ad unde cupiditate error a obcaecati. Quaerat delectus autem, eius ex nisi architecto vero corporis amet 
+        sapiente at illum culpa laudantium quis a laborum inventore officiis consequuntur debitis id.
+        Ipsam dicta eos earum optio cupiditate quisquam, ratione officia temporibus amet qui provident hic quibusdam ex laudantium 
+        voluptate autem! Suscipit laudantium nostrum fugiat facere ipsam eligendi ab voluptatem, sequi quas.
+        Sequi totam eius deserunt tempore. Ipsa in vitae impedit aliquid voluptatum nesciunt ea eveniet, consectetur quasi, atque 
+        rem enim sed voluptas placeat animi! Labore esse iusto magnam et illum minima.
+      </p>
+    </div>    
 </section>
 </body>
 
-<!-- Javier MILEI -->
+<!-- Javier MILEI buscar info -->
 
 <?php require($_SERVER['DOCUMENT_ROOT'] . "/core/footer.php"); ?>
